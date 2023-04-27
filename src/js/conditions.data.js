60bf392b41f98924062e99ce5f401be229a7e223
Anbu <15264938+anburocky3@users.noreply.github.com>
2023-04-27 19:08:27 +0530

    feat: dynamic conditions

---
+++ src/js/conditions.data.js
@@ -0,0 +1,9 @@
+const conditionList = [
+  'You have to work Min. 6 hours every day.',
+  'This program is only for beginners, You shouldn’t work in any other companies.',
+  'You should have whatsapp/laptop/internet with webcam access. You should be available anytime durning the working hours.',
+  'Once shortlisted candidates, will have a video call, They have to present why they should be in this program.',
+  'The selected candidates will be invited to group where the internship program continues.',
+]
+
+export default conditionList
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
 const mainEl = document.querySelector('main')
 const successfulUIEl = document.getElementById('formSubmitted')
 const formEl = document.querySelector('form')
+import conditionList from './conditions.data.js'
+const conditionContainerEl = document.getElementById('conditions')
+
+// console.log(conditionList)
+;(function injectConditions() {
+  const fragment = document.createDocumentFragment()
+
+  // looping all condition list
+  conditionList.forEach((condition) => {
+    const liEl = document.createElement('li')
+    liEl.textContent = condition
+    fragment.appendChild(liEl)
+  })
+  conditionContainerEl.appendChild(fragment)
+})()
 
 const validateForm = () => {
   const formInputsEl = formEl.querySelectorAll('input, select, textarea')
